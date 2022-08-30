67fe4603ca6ce8a3a4f0942a98c0a265bf4d554a
GitHappyGod <chami0205@gmail.com>
2022-08-30 15:05:06 +0900

    privacy

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://winter:1234@winterlimited.umweikt.mongodb.net/?retryWrites=true&w=majority'
+}
---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+} //heroku 업로드할때 MONGO_URI 변수이름 같게 입력해서 넣어놓기
